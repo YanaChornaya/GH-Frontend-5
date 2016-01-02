97fa0126b5fdb7aa27c7222bd2b04b5519b33089
YanaChornaya <yarina297@gmail.com>
2016-01-02 15:36:41 +0200

    Homework11

---
+++ Lesson11/Brandi/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $("#owl-example-hero").owlCarousel({
+    singleItem: true,
+    autoPlay: true,
+    transitionStyle : "fade",
+  });
+
+  $("#owl-example").owlCarousel({
+  	singleItem: true,
+    transitionStyle : "backSlide",
+  });
+
+  $("#owl-example1").owlCarousel({
+    singleItem: true,
+    transitionStyle : "backSlide",
+  });
+ 
+});
